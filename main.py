c560847355b92c5d585603b900d46129f4e0fbfe
Алексей Даниленко <alexdaniel76@gmail.com>
2022-10-02 22:04:33 +0300

    change all

--- main.py
+++ main.py
@@ -1,65 +1,73 @@
-# Генератор пароля
-# Выбор длины пароля от минимального 4 до 128
-# Выбор англисукого алфавита, цифр и символов
-# Не повторяемость символов
-
-# Импорт генерация случайных чисел (модуль random)
+# Генератор безопасных паролей
 import random
 
-# Создаем списки
-pass_little = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-]
-pass_big = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-]
-pass_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-pass_symbols = [
-    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[',
-    ']', '{', '}', ';', ':', '/', '?', '.', '>', '<'
-]
-user_password = []
 
-print('''Выберите длину пароля. Минимальная длина 4 символа,
-    максимальная длина 128 символов.
-        ''')
+def generate_password(length, chars):
+    """
+    Создаем случайный пароль на основе выбранной длина и набора символов
+    Аргументы:
+    length - Длина каждого пароля
+    symbols - Список символов используемых для создания пароля
+    """
+    user_password = ""  # Новый список для пароля
+    for _ in range(length):
+        user_password += random.choice(chars)
+    print("Пароль: ", user_password)
 
-# Проверка длина пароля
-for _ in range(5):  # Проверяем 5 раз
-    pass_length = int(input("Длина: "))
-    if pass_length > 128 or pass_length < 4:
-        print('Минимальная длина пароля 4 символа, максимальная 128')
-        pass_length = 4
+
+# Создайте строковые константы
+digits = "0123456789"
+lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+punctuation = "!#$%&*+-=?@^_"
+
+print("Генератор безопасных паролей")
+# Запрос на количество генерируемых паролей
+count = int(input("Выберите количество паролей: "))
+if count <= 0:  # Если количество паролей меньше 1
+    count = 1
+    print("Количество паролей не может быть меньше 1")
+
+# Запрос на длину пароля
+length = int(input("Выберите длину пароля: "))
+
+# Выбор состава символов для пароля
+while True:  # Основной цикл
+    chars = ""  # Пустая переменная для символов
+    print("Выберете состав пароля")
+    pass_type = str(input("- Пароль включает строчные символов (abc...)? (Д / Н): "))
+    if pass_type == "Д" or pass_type == "д":
+        chars += lowercase_letters
+
+    pass_type = str(input("- Пароль включает прописные символов (ABC...)? (Д / Н): "))
+    if pass_type == "Д" or pass_type == "д":
+        chars += uppercase_letters
+
+    pass_type = str(input("- Пароль включает числа (123...)? (Д / Н): "))
+    if pass_type == "Д" or pass_type == "д":
+        chars += digits
+
+    pass_type = str(
+        input("- Пароль включает специальные символы (! @ # ...)? (Д / Н): ")
+    )
+    if pass_type == "Д" or pass_type == "д":
+        chars += punctuation
+
+    if chars == "":  # Проверяем выбрали символы
+        print("Не выбраны символы для пароля!")
+        continue
+
+    similar_symbols = input("Исключать ли неоднозначные символы il1Lo0O? (Д / Н): ")
+    if similar_symbols == "Д" or similar_symbols == "д":
+        for c in "il1Lo0O":
+            chars = chars.replace(c, "")
+
+    for _ in range(count):  # Цикл по количеству паролей
+        generate_password(length, chars)  # Создаем пароль
+
+    # Запрос на выход
+    exit = input("Хотите закрыть программу? (Д / Н)) ")
+    if exit == "Н" or exit == "н":
+        continue
     else:
         break
-
-print('''Пароль состоит из символов:
-        строчных (abc...)   - 1
-        прописных (ABC...), строчных  - 2
-        чисел (123...),  прописных, строчных - 3
-        специальные (! @ # ...), чисел, прописных, строчных - 4
-        ''')
-pass_type = int(input('Выберите состав: '))
-
-# Выбор состава
-if pass_type == 1:
-    pass_base = pass_little
-elif pass_type == 2:
-    pass_base = pass_little + pass_big
-elif pass_type == 3:
-    pass_base = pass_little + pass_big + pass_numbers
-else:
-    pass_base = pass_little + pass_big + pass_numbers + pass_symbols
-
-# Генерация пароля
-for i in range(pass_length):
-    user_password.append(random.choice(pass_base))
-
-user_password = ''.join(user_password)
-print('Пароль: ', user_password)
-
-# Для показа термина после расчетов
-print()
-input('Для закрытия нажмите Enter')
